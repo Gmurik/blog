4b952d8864cd567e2f642a1bc99d6db9a89139c8
VShafeeva <rolf_73@mail.ru>
2019-01-31 21:31:06 +0400

    dz 3 что успел

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="assets/css/default.css?cbcabb7557075c34568079062e82823cf039b5e6"/>
     @section('head_style')
     @show
-    <!--[if lt IE 9]>
+    <!--[if lt IE 9 ]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
